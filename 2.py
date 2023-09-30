07b480d1dcb6f2d3d90237c20699c2c3fc6eef1a
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-09-30 13:01:02 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,4 +1,16 @@
-from itertools import *
+# from itertools import *
 
-x = [a for a in input().split()]
-print(max(list(permutations(x, r=len(x)))))
+# x = [a for a in input().split()]
+# print(max(list(permutations(x, r=len(x)))))
+
+# x = [str(x) for x in input().split()]
+# x = sorted(x, reverse=True)
+lst = []
+while True:
+    n = input()
+    if not n:
+        break
+    lst.append(n)
+
+lst.sort(reverse=True)
+print(''.join(lst))
