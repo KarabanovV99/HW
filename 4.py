df1ce404be6c0097de9282bcc0e5a0a051fded91
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-09-30 12:55:57 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -1,14 +1,7 @@
 x = input()
-k = 0 
-b = 0
-for i in range(len(x)):
-    if x[i] == '(':
-        k += 1
-    if x[i] == ')':
-        b += 1
-if k < b:
+if x.count('(') < x.count(')'):
     print("Не хватает '('")
-elif k > b:
+elif x.count('(') > x.count(')'):
     print("Не хватает ')'")
 else:
     print("Все скобки есть")
