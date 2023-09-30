1bfea1532da38b23bef2de29f29fce812ea3aca3
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-09-30 13:13:39 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,7 +1,9 @@
 x = input()
-if x.count('(') < x.count(')'):
+a = x.count('(')
+b = x.count(')')
+if a < b:
     print("Не хватает '('")
-elif x.count('(') > x.count(')'):
+elif a > b:
     print("Не хватает ')'")
 else:
     print("Все скобки есть")
