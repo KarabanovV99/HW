ed510742472151cb2794bc5c23d1516560ec16d2
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-09-30 13:07:55 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -19,13 +19,11 @@
 # print(lst2[m - 1])
 
 x = input()
-y = int(input())
+y = y0 = int(input())
 a = 0
 for i in x:
     if i.isdigit():
-        a += 1
-        if a == y:
-            y -= 1
+        y -= 1
         if not y:
-            print(y, "-ая цифра в строке:", i)
+            print(y0, "-ая цифра в строке:", i)
             break
