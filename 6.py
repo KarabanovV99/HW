91226a83331e8808073f705e7a976087e4ac797d
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-09-30 12:50:48 +0300

    Add files via upload

--- 6.py
+++ 6.py
@@ -1,10 +1,31 @@
-lst = []
-while True:
-    n = input()
-    if n == "":
-        break
-    lst.append(n)
-m = int(input())
-lst1 = [''.join(map(str, lst))]
-f = str(lst1)
-print(f[m + 1])
+# lst = []
+# lst2 = []
+
+# while True:
+#     n = input()
+#     if not n:
+#         break
+#     lst.append(n)
+
+# m = int(input())
+# lst1 = [''.join(lst)]
+# # lst1 = [''.join(map(str, lst))]
+# # y = input()
+# #y = lst1[0]
+# for x in y:
+#     # if x in '1234567890':
+#     if x.isdigit():
+#         lst2.append(x)
+# print(lst2[m - 1])
+
+x = input()
+y = int(input())
+a = 0
+for i in x:
+    if i.isdigit():
+        a += 1
+        if a == y:
+        y -= 1
+        if not y:
+            print(y, "-ая цифра в строке:", i)
+            break
