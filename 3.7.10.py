0854c6cc5e62b71fc37e30a3f23f7bf05c7c8c25
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-10-07 02:09:39 +0300

    .

---
+++ 3.7.10.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def long(t):
+
+    longest = max(t, key=len)
+    
+    return longest
+
+def coun(t):
+
+    count = Counter(t)
+    return count.most_common(1)[0][0]
+
+lst = input("Введите список: ").split()
+
+print(long(lst), coun(lst))
