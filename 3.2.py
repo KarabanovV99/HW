0de932b4f1b71e8cb3c93c14864d3dc407c95f8b
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-10-07 02:17:03 +0300

    Update 3.2.py

--- 3.2.py
+++ 3.2.py
@@ -1,10 +1,10 @@
-def f(n):
+def season(n):
     if 1 <= n <= 2 or n == 12:
         return "зима"
-    if 3 <= n <= 5:
+    elif 3 <= n <= 5:
         return "весна"
-    if 6 <= n <= 8:
+    elif 6 <= n <= 8:
         return "лето"
-    if 9 <= n <= 11:
+    else 9 <= n <= 11:
         return "осень"
-print(f(int(input())))
+print(season(int(input())))
