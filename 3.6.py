f186350c8b6b00038bc3de8bc7d94c6637b72bef
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-10-07 01:27:59 +0300

    .

--- 3.6.py
+++ 3.6.py
@@ -1,15 +1,14 @@
-def f(n):
-    lst = []
-    for i in range(1, int(n**0.5) + 1):
-        if n % i == 0:
-            lst.append(i)
-            lst.append(n // i)
-    lst = set(lst)
-    if len(lst) == 2:
-        return True
-    else:
-        return False
+def prosto(n):
 
+    c = 0
+
+    for i in range(1, int(n**0.5) + 1):
+        if int(n) % i == 0:
+            c += 1
+            if c > 1:
+                return False
+    return True
+        
 x = int(input())
 
-print(f(x))
+print(prosto(x))
