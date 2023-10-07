3b41bd2e950b8649f1dc65463d8ae686101dde18
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-10-08 01:07:51 +0300

    4

--- 2.py
+++ 2.py
@@ -1,16 +1,7 @@
-# from itertools import *
-
-# x = [a for a in input().split()]
-# print(max(list(permutations(x, r=len(x)))))
-
-# x = [str(x) for x in input().split()]
-# x = sorted(x, reverse=True)
-lst = []
-while True:
-    n = input()
-    if not n:
-        break
-    lst.append(n)
-
-lst.sort(reverse=True)
-print(''.join(lst))
+def fak(n):
+    c = 1
+    for i in range(1, n + 1):
+        c = c * i
+    return c
+ 
+print(fak(0))
