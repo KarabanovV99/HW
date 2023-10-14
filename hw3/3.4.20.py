b760558a8da9a07ce2a0e9a2905e3aa5291a1311
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-10-14 11:35:19 +0300

    ///

--- hw3/3.4.20.py
+++ hw3/3.4.20.py
@@ -1,14 +1,17 @@
 from collections import Counter
+from one import vvod
+
 
 def f(lst):
     sorted_counter = Counter(lst).most_common()
     output = ["Элемент | Частота"]
-    
+
     for e, c in sorted_counter:
         output.append(f"{e.ljust(7)} | {c:4}")
-    
-    return '\n'.join(output)
-  
-lst = [x.strip() for x in input("Введите список: ").split(",")]
 
-print(f(lst))
+    return '\n'.join(output)
+
+
+
+
+print(f(vvod()))
