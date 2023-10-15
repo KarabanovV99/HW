23e6607b082320f2396e14da9022fe059832e130
KarabanovV99 <143869788+KarabanovV99@users.noreply.github.com>
2023-10-15 22:34:22 +0300

    2 дополнительное

--- hw3/2.bonus.py
+++ hw3/2.bonus.py
@@ -1,36 +1,3 @@
-# from itertools import permutations
-
-
-# def sled(chislo):
-#     lst = []
-#     for i in permutations(chislo):
-#         lst.append(i)
-#     chislo = int(chislo)
-#     sorted_lst = sorted(lst, key=lambda x: abs(x - chislo))
-#     for n in sorted_lst:
-#         if n > chislo:
-#             return n
-#     return None
-
-
-# print(sled("21"))
-
-# from itertools import permutations
-
-# def sled(chislo):
-#     lst = []
-#     for i in permutations(chislo):
-#         lst.append(int(''.join(i)))
-#     chislo = int(chislo)
-#     sorted_lst = sorted(lst)
-#     for n in sorted_lst:
-#         if n > chislo:
-#             return str(n)
-#     return None
-
-# print(sled("123456"))
-
-
 from itertools import permutations
 
 
