21780d08f3ec3e33898131085868ccbb6fd945ea
BashKill <143869788+KarabanovV99@users.noreply.github.com>
2024-03-15 00:15:38 +0300

    Add files via upload

--- HW10/date/main_date.py
+++ HW10/date/main_date.py
@@ -11,30 +11,45 @@ def main():
     elif voite == 2:
         d1 = DateStamp()
     else:
-        print(ColorEnum.RED + "Incorrect choice. Try again" + ColorEnum.FINISH)
+        print("{0}Incorrect choice. Try again{1}".format(ColorEnum.RED, ColorEnum.FINISH))
     print(ColorEnum.GREEN + "\n" + str(d1) + ColorEnum.FINISH)
     while True:
-        print(ColorEnum.BLUE + "_" * 27 + ColorEnum.FINISH, "\n")
+        print(ColorEnum.BLUE + "_" * 27 + ColorEnum.FINISH, '\n')
         print(ColorEnum.GREEN + "1. Отняти или добавить дни")
         print("2. Вычислить день недели")
-        print("3. Exit" + ColorEnum.FINISH, "\n")
+        print("3. Сложить или вычесть даты")
+        print("4. Exit" + ColorEnum.FINISH, "\n")
 
-        choice = input(ColorEnum.YELLOW + "Enter your choice: " + ColorEnum.FINISH)
+        choice = input("{0}Enter your choice: {1}".format(ColorEnum.YELLOW, ColorEnum.FINISH))
         print("")
 
         if choice == '1':
-            num = int(input(ColorEnum.YELLOW + "Введите количество дней для корректировки: " + ColorEnum.FINISH))
+            num = int(input(
+                "{0}Введите количество дней для корректировки: {1}".format(ColorEnum.YELLOW, ColorEnum.FINISH)))
             d1.adjust_days(num)
-            print("\n", ColorEnum.GREEN + "Новая дата после корректировки: " + str(d1) + ColorEnum.FINISH)
+            print("\n", "{0}Новая дата после корректировки: {1}{2}".format(ColorEnum.GREEN, str(d1), ColorEnum.FINISH))
 
         elif choice == '2':
-            print(ColorEnum.GREEN + d1.day_of_the_week() + ColorEnum.FINISH)
+            print("{0}{1}{2}".format(ColorEnum.GREEN, d1.day_of_the_week(), ColorEnum.FINISH))
 
         elif choice == '3':
+            choice_one = input("{0}Введите + для сложения и - для вычитания: {1}".format(ColorEnum.YELLOW, ColorEnum.FINISH))
+            if choice_one == '+':
+                d2 = Date()
+                d2.input_date()
+                print(d1 + d2)
+            elif choice_one == '-':
+                d2 = Date()
+                d2.input_date()
+                print(d1 - d2)
+            else:
+                print("{0}Incorrect choice{1}".format(ColorEnum.RED, ColorEnum.FINISH))
+
+        elif choice == '4':
             break
 
         else:
-            print(ColorEnum.RED + "Incorrect choice. Try again" + ColorEnum.FINISH)
+            print("{0}Incorrect choice. Try again{1}".format(ColorEnum.RED, ColorEnum.FINISH))
 
 
 if __name__ == "__main__":
